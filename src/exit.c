a3cb34cf676c2da400d17ba420b46067e4fc5483
Jeremy Coquet <jcoquet@c1r5p6.42quebec.com>
2024-05-17 10:20:00 -0400

    main

--- src/exit.c
+++ src/exit.c
@@ -6,8 +6,15 @@
 /*   By: jcoquet <jcoquet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 16:00:00 by jcoquet           #+#    #+#             */
-/*   Updated: 2024/05/17 08:15:47 by jcoquet          ###   ########.fr       */
+/*   Updated: 2024/05/17 10:12:48 by jcoquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	ft_exit(char *input)
+{
+	free(input);
+	printf("%sexit minishell\n%s", RED, RST);
+	exit(EXIT_SUCCESS);
+}
