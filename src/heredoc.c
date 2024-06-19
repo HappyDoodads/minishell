d5e781ee25007989a76362f0eefeda8989d30cf5
Jeremy Coquet <jcoquet@c1r5p6.42quebec.com>
2024-06-19 15:53:21 -0400

    heredoc

---
+++ src/heredoc.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcoquet <jcoquet@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 12:01:48 by jcoquet           #+#    #+#             */
+/*   Updated: 2024/06/19 08:33:14 by jcoquet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_heredoc(t_command *cmd)
+{
+	int fd;
+	int i = 1; // Index a passer en pointeur lors du parsing
+	char *input;
+	char *file = ft_strjoin (".tmpfile", ft_itoa(i)); // creation tmp file pour heredoc
+	if (cmd->argv[1] == NULL)
+	{
+		ft_dprintf(2, "minishell: syntax error\n");
+		return (EXIT_FAILURE);
+	}
+	fd = open(file, O_CREAT | O_RDWR | O_APPEND, 0644);
+	while ((1))
+	{
+		input = readline("> ");
+		if (strncmp(input, cmd->argv[1], ft_strlen(cmd->argv[1])) == 0)
+		{
+			close(fd);
+			free(input);
+			break ;
+		}
+		ft_dprintf(fd, "%s\n", input);
+			free(input);
+	}
+	return (0);
+}
