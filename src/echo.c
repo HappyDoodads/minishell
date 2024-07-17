90824d52d39e5f2ec3ffd4b025b6ab9989171ba6
Jeremy Coquet <jcoquet@c1r5p6.42quebec.com>
2024-07-17 17:16:15 -0400

    echo v2

--- src/echo.c
+++ src/echo.c
@@ -6,48 +6,59 @@
 /*   By: jcoquet <jcoquet@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 17:47:54 by jdemers           #+#    #+#             */
-/*   Updated: 2024/07/17 14:09:24 by jcoquet          ###   ########.fr       */
+/*   Updated: 2024/07/17 17:13:06 by jcoquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_echo(t_command *cmd, t_misc *misc)
+static int option_n(t_command *cmd, unsigned int *i, char *end)
 {
-	unsigned int	i;
 	unsigned int	j;
-	int flag = 2;
-	char			end;
+	int				flag;
 
-	(void)misc;
-	i = 1;
-	end = '\n';
-	while (cmd->argv[i] && !ft_strncmp(cmd->argv[i], "-n", 2))
+	flag = 2;
+	while (cmd->argv[*i] && !ft_strncmp(cmd->argv[*i], "-n", 2))
 	{
 		j = 2;
-		end = 0;
-		while (cmd->argv[i][j])
+		*end = 0;
+		while (cmd->argv[*i][j])
 		{
-			if (cmd->argv[i][j] != 'n' && cmd->argv[i][j] != '\0')
+			if (cmd->argv[*i][j] != 'n' && cmd->argv[*i][j] != '\0')
 			{
-				flag = i;
+				flag = *i;
 				break ;
 			}
 			j++;
 		}
-		i++;
 		if (flag != 2)
-			break ;
+            break ;
+		(*i)++;
 	}
+	return (flag);
+}
+
+int	ft_echo(t_command *cmd, t_misc *misc)
+{
+	unsigned int	i;
+	char			end;
+	int 			flag;
+
+	(void)misc;
+	i = 1;
+	end = '\n';
+	flag = option_n(cmd, &i, &end);
 	if (end == 0)
-		i = flag;
-	while (cmd->argv[i] != NULL)
-	{
-		ft_putstr_fd(cmd->argv[i++], cmd->pipe_r[1]);
-		if (cmd->argv[i] != NULL)
-			ft_putchar_fd(' ', cmd->pipe_r[1]);
-	}
-	if (end)
-		ft_putchar_fd(end, cmd->pipe_r[1]);
-	return (0);
+        i = flag;
+	else
+		i = 1;
+	 while (cmd->argv[i] != NULL)
+    {
+        ft_putstr_fd(cmd->argv[i++], cmd->pipe_r[1]);
+        if (cmd->argv[i] != NULL)
+            ft_putchar_fd(' ', cmd->pipe_r[1]);
+    }
+    if (end)
+        ft_putchar_fd(end, cmd->pipe_r[1]);
+    return (0);
 }
