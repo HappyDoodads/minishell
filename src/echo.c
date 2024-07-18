b5914ac856dd1d9532ec486d51866c114ddd03cf
Jeremy Coquet <jcoquet@c1r5p6.42quebec.com>
2024-07-18 11:59:51 -0400

    echo ok

--- src/echo.c
+++ src/echo.c
@@ -6,59 +6,63 @@
 /*   By: jcoquet <jcoquet@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 17:47:54 by jdemers           #+#    #+#             */
-/*   Updated: 2024/07/17 17:13:06 by jcoquet          ###   ########.fr       */
+/*   Updated: 2024/07/18 11:58:31 by jcoquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int option_n(t_command *cmd, unsigned int *i, char *end)
+static int	check_option(t_command *cmd, int i)
 {
-	unsigned int	j;
-	int				flag;
+	int	j;
 
-	flag = 2;
-	while (cmd->argv[*i] && !ft_strncmp(cmd->argv[*i], "-n", 2))
+	j = 0;
+	if (cmd->argv[i][j] == '-')
 	{
-		j = 2;
-		*end = 0;
-		while (cmd->argv[*i][j])
+		j++;
+		while (cmd->argv[i][j])
 		{
-			if (cmd->argv[*i][j] != 'n' && cmd->argv[*i][j] != '\0')
-			{
-				flag = *i;
+			if (cmd->argv[i][j] != 'n')
 				break ;
-			}
 			j++;
 		}
-		if (flag != 2)
-            break ;
-		(*i)++;
 	}
-	return (flag);
+	return (j);
+}
+
+static	void	write_echo(t_command *cmd, int i, char end)
+{
+	while (cmd->argv[i] != NULL)
+	{
+		ft_putstr_fd(cmd->argv[i++], cmd->pipe_r[1]);
+		if (cmd->argv[i] != NULL)
+			ft_putchar_fd(' ', cmd->pipe_r[1]);
+	}
+	if (end)
+		ft_putchar_fd(end, cmd->pipe_r[1]);
 }
 
 int	ft_echo(t_command *cmd, t_misc *misc)
 {
-	unsigned int	i;
-	char			end;
-	int 			flag;
+	int		i;
+	int		j;
+	char	end;
 
-	(void)misc;
 	i = 1;
 	end = '\n';
-	flag = option_n(cmd, &i, &end);
-	if (end == 0)
-        i = flag;
-	else
-		i = 1;
-	 while (cmd->argv[i] != NULL)
-    {
-        ft_putstr_fd(cmd->argv[i++], cmd->pipe_r[1]);
-        if (cmd->argv[i] != NULL)
-            ft_putchar_fd(' ', cmd->pipe_r[1]);
-    }
-    if (end)
-        ft_putchar_fd(end, cmd->pipe_r[1]);
-    return (0);
+	(void)misc;
+	while (cmd->argv[i])
+	{
+		j = check_option(cmd, i);
+		if (cmd->argv[i][j] == '\0')
+		{
+			i++;
+			j = 0;
+			end = 0;
+		}
+		else
+			break ;
+	}
+	write_echo(cmd, i, end);
+	return (0);
 }
